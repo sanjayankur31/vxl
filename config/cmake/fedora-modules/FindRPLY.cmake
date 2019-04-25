aacd92f275ad6f78c8d13441e33baf36161318e6
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2019-04-25 14:11:31 +0100

    v2.0.2 Add cmake module to find RPLY

---
+++ config/cmake/fedora-modules/FindRPLY.cmake
@@ -0,0 +1,39 @@
+# Adapted from https://github.com/jirihnidek/verse-ply-uploader/blob/master/build_files/cmake/modules/FindRPLY.cmake
+# This module tries to find RPLY library and include files
+#
+# RPLY_INCLUDE_DIR, where to find verse.h
+# RPLY_LIBRARY_DIR, where to find libverse.so
+# RPLY_LIBRARIES, the library to link against
+# RPLY_FOUND, IF false, do not try to use Verse
+#
+
+FIND_PATH ( RPLY_INCLUDE_DIR rply.h
+    /usr/include
+    /usr/include/rply
+    /usr/local/include
+    /usr/local/include/rply
+)
+
+FIND_LIBRARY ( RPLY_LIBRARIES rply
+    /usr/local/lib
+    /usr/local/lib64
+    /usr/lib
+    /usr/lib64
+)
+
+GET_FILENAME_COMPONENT( RPLY_LIBRARY_DIR ${RPLY_LIBRARIES} PATH )
+
+SET ( RPLY_FOUND "NO" )
+IF ( RPLY_INCLUDE_DIR )
+    IF ( RPLY_LIBRARIES )
+        SET ( RPLY_LIBRARY ${RPLY_LIBRARIES} )
+        SET ( RPLY_FOUND "YES" )
+    ENDIF ( RPLY_LIBRARIES )
+ENDIF ( RPLY_INCLUDE_DIR )
+
+MARK_AS_ADVANCED (
+    RPLY_LIBRARY_DIR
+    RPLY_INCLUDE_DIR
+    RPLY_LIBRARIES
+    RPLY_LIBRARY
+)
